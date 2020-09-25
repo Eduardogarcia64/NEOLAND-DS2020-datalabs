16928ccb643e78a71066eee3b51917e87a65b050
Edu <luiseduardoorozcogarcia@gmail.com>
2020-09-25 18:04:55 +0200

    nice

--- 03.test_edu.py
+++ 03.test_edu.py
@@ -10,4 +10,13 @@ dinner = int(input())
 print('Number of calories in your snacks')
 snack = int(input())
 totalCalories = breakfast + lunch + dinner + snack
-print('Calories of '+date +' are: '+str(totalCalories))
+print('Calories of '+date +' are: '+str(totalCalories));
+
+respuesta = input('Would you like to continue?')
+if respuesta == 'Yes' or respuesta =='y':
+    print('Continuing...')
+    print('complete')
+elif respuesta == 'no' or respuesta == 'n':
+    print('Existing')
+else: 
+    print('Try answering Yes or no')
